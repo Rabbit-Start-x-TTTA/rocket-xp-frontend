0835b055dd1091ccf0a9b1f19714108de0367e19
tao-isaman <towlovelove@gmail.com>
2025-06-26 21:36:34 +0700

    feat: init xp earning page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="min-h-screen flex flex-col justify-between bg-gradient-to-br from-[#fff] to-[#F8F4FF]">
       <div className="flex flex-col items-center justify-center flex-1 pt-24">
         <Image src="/logo.png" alt="Rocket XP Logo" width={180} height={180} priority className="mb-6" />
-        <h1 className="text-6xl font-extrabold gradient-text mb-2 tracking-tight">ROCKET XP</h1>
+        <h1 className="text-6xl font-extrabold gradient-text mb-2 tracking-tight">ROCKET XP 2</h1>
         <div className="text-lg font-semibold text-black mb-12 tracking-wide">RABBIT START CO., LTD.</div>
       </div>
       <div className="w-full flex justify-center pb-12">
---
+++ src/app/xp-earning/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function XpEarningPage() {
+  return (
+    <div>
+      <h1>Xp Earning</h1>
+    </div>
+  )
+}
