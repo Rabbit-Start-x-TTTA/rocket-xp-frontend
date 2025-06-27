f3a63857864edf73f1557bebf23765f660f02438
Bazkiller <ohm.ratchaphon@gmail.com>
2025-06-27 20:24:28 +0700

    feat: create search area with search button

--- src/app/product-catalog/page.tsx
+++ src/app/product-catalog/page.tsx
@@ -3,14 +3,16 @@ import Image from "next/image";
 export default function ProductCatalogPage() {
   return (
     <>
-      <main>
-        <section>
+      <main className="width-full flex flex-col items-center justify-center p-4">
+        <section className="w-full mx-4 mb-4 flex">
           <input
             type="text"
             placeholder="Search products..."
-            className="border p-2"
+            className="flex-1 border border-gray-300 rounded-l-lg px-4 py-2 focus:outline-none focus:ring-1 focus:ring-[#C6639D] focus:border-transparent"
           />
-          <button className="bg-blue-500 text-white p-2">Search</button>
+          <button className="bg-[#524389] hover:bg-[#C6639D] text-white px-6 py-2 rounded-r-lg flex items-center">
+            Search
+          </button>
         </section>
         <section>
           <div>
