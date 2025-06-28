1aee456ee17c627684431b546fe1fbb35b0d4263
Bazkiller <ohm.ratchaphon@gmail.com>
2025-06-28 20:42:59 +0700

    feat: make dynamic product component and use image instead of dummy div tag

--- src/app/product-catalog/product.tsx
+++ src/app/product-catalog/product.tsx
@@ -1,10 +1,24 @@
-export default function Product() {
+import Image from "next/image";
+
+export default function Product({
+  name = "Product Name",
+  price = 0,
+  img_path = "/book.jpg",
+}: {
+  name?: string;
+  price?: number;
+  img_path?: string;
+}) {
   return (
     <li className="border border-gray-300 rounded-md overflow-hidden">
-      <div className="w-[150px] h-[150px] bg-gray-200"></div>
+      <div className="relative min-w-[175px] h-[150px] bg-gray-200">
+        <Image src={img_path} alt={name} fill />
+      </div>
       <div className="p-3">
-        <h2 className="text-gray-900 mb-1">Product</h2>
-        <p className="text-[#D63AA2] text-lg font-semibold">$99.00</p>
+        <h2 className="text-gray-900 mb-1">{name}</h2>
+        <p className="text-[#D63AA2] text-lg font-semibold">
+          ${price?.toFixed(2)}
+        </p>
       </div>
     </li>
   );
--- src/app/product-catalog/productGrid.tsx
+++ src/app/product-catalog/productGrid.tsx
@@ -1,16 +1,39 @@
 import Product from "./product";
 
+const data: any[] = [
+  {
+    id: 1,
+    name: "Product 1",
+    price: 99.0,
+    img_path: "/book.jpg",
+  },
+  {
+    id: 2,
+    name: "Product 2",
+    price: 89.0,
+    img_path: "/book.jpg",
+  },
+  {
+    id: 3,
+    name: "Product 3",
+    price: 79.0,
+    img_path: "/book.jpg",
+  },
+  {
+    id: 4,
+    name: "Product 4",
+    price: 69.0,
+    img_path: "/book.jpg",
+  },
+];
+
 export default function ProductGrid() {
   return (
     <section>
-      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+        {data.map((product) => (
+          <Product key={product.id} {...product} />
+        ))}
       </ul>
     </section>
   );
