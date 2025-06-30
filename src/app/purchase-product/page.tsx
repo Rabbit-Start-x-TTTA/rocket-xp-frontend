6373baed19497c5d8b8f6d9e9539031790c6e94f
Bazkiller <ohm.ratchaphon@gmail.com>
2025-06-30 16:04:53 +0700

    feat: create page.tsx in purchase-pruduct folder

---
+++ src/app/purchase-product/page.tsx
@@ -0,0 +1,9 @@
+export default function PurchaseProductPage() {
+  return (
+    <div>
+      <h1>Purchase Product</h1>
+      <p>This is the purchase product page.</p>
+      <p>Here you can implement the logic to handle product purchases.</p>
+    </div>
+  );
+}
