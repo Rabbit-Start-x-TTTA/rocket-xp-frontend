fcf02c0ba288175122e9ee20157237442970c3ee
Bazkiller <ohm.ratchaphon@gmail.com>
2025-06-30 22:55:18 +0700

    feat: create dummy side navigator bar

--- src/app/event-management/page.tsx
+++ src/app/event-management/page.tsx
@@ -1,8 +1,13 @@
 export default function EventManagementPage() {
   return (
-    <main className="min-h-screen p-4 text-black">
-      <h1 className="font-bold text-2xl">Event Management</h1>
-      <p className="mt-4">Manage your events efficiently.</p>
-    </main>
+    <div className="flex min-h-screen">
+      <nav className="bg-[#D63AA2] flex justify-center items-center w-64 text-white text-3xl">
+        Side Navbar
+      </nav>
+      <main className="min-h-screen p-4 text-black">
+        <h1 className="font-bold text-3xl">Event Management</h1>
+        <p className="mt-4">Manage your events efficiently.</p>
+      </main>
+    </div>
   );
 }
