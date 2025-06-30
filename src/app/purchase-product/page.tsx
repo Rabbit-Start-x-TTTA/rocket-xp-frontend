e91bde0dd36fc34cf7b76e81bc92243f35462ccf
Bazkiller <ohm.ratchaphon@gmail.com>
2025-06-30 22:38:05 +0700

    feat: create purchase-product page

--- src/app/purchase-product/page.tsx
+++ src/app/purchase-product/page.tsx
@@ -1,9 +1,34 @@
 export default function PurchaseProductPage() {
   return (
-    <div>
-      <h1>Purchase Product</h1>
-      <p>This is the purchase product page.</p>
-      <p>Here you can implement the logic to handle product purchases.</p>
-    </div>
+    <main className="min-h-screen p-4 text-black">
+      <p className="pb-6">BACK</p>
+      <h1 className="font-bold text-2xl">สินค้าในตะกร้า</h1>
+
+      <ul className="mt-10 mb-20 space-y-2">
+        <li className="flex justify-between">
+          <p>item 1</p>
+          <p>99$</p>
+        </li>
+        <li className="flex justify-between">
+          <p>item 2</p>
+          <p>99$</p>
+        </li>
+        <li className="flex justify-between ">
+          <p>item 3</p>
+          <p>99$</p>
+        </li>
+      </ul>
+      <div className="flex justify-between">
+        <p>Discount</p>
+        <p className="bg-purple-500 text-black p-2">TICKET</p>
+      </div>
+      <div className="flex justify-between font-bold text-xl mt-4">
+        <p>Total</p>
+        <p>297$</p>
+      </div>
+      <button className="w-full bg-purple-500 text-black p-2 mt-4 hover:bg-purple-100 transition-colors">
+        ชำระเงิน
+      </button>
+    </main>
   );
 }
