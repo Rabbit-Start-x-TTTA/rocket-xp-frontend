0918402e5c21bffaa0eaa2d70f76e946d0e5320f
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-07-02 01:55:41 +0700

    feat(colors): add centralized color definition file

---
+++ src/styles/color.ts
@@ -0,0 +1,16 @@
+const colors = {
+  primary: {
+    purple: "#524389",
+    pink: "#D63AA2",
+  },
+  secondary: {
+    pink: "#C6639D",
+    lightBlue: "#4891C6",
+    yellow: "#FFB519",
+    darkPurple: "#382E5E"
+  },
+  white: "#FFFFFF",
+  black: "#000000"
+}
+
+export default colors
