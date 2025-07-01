5e48c6103f9e4fbb079cbdc0481779d9b004d605
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-07-02 01:25:09 +0700

    feat: init interest mapping page

---
+++ src/app/interest-mapping/page.tsx
@@ -0,0 +1,8 @@
+// import InterestPicker from "./InterestPicker" 
+
+export default function InterestMappingPage() {
+return (
+    <div className="p-8">
+      <h1 className="text-xl font-bold">Pick your interests</h1>
+    </div>
+  )}
