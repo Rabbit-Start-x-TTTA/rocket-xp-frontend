22b9a17baa306ca256a2bc6dbdc46d54d5d27aec
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-01 13:18:48 +0700

    feat: create all product-management page

---
+++ src/app/product-management/page.tsx
@@ -0,0 +1,8 @@
+export default function ProductManagementPage() {
+  return (
+    <div>
+      <h1>Product Management</h1>
+      {/* Add your product management components here */}
+    </div>
+  );
+}
