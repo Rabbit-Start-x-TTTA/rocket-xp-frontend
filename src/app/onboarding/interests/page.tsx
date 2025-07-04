18750c70a918de247d085d2da9b5307ce6000fad
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-05 01:18:07 +0700

    style: fix max width instead of fix width directly

--- src/app/onboarding/interests/page.tsx
+++ src/app/onboarding/interests/page.tsx
@@ -4,27 +4,29 @@ import Link from "next/link";
 
 export default function InterestsPage() {
   return (
-    <main className="min-h-screen w-[393px] mx-auto ">
+    <main className="min-h-screen max-w-[393px] mx-auto border-x">
       <h1 className="text-xl font-bold bg-[#D9D9D9] text-center py-4 mb-2">
         ความสนใจ
       </h1>
-      <h2 className="mb-6">สามารถเลือกได้สูงสุด 5 ความสนใจ</h2>
-      <InterestsGrid interests={interests} />
-      <div className="flex items-center justify-center gap-2 mt-4">
-        <input type="checkbox" name="accept" />
-        <p>
-          <span className="opacity-70">ข้าพเจ้ายอมรับ</span>
-          &nbsp;
-          <Link href="#" className="hover:underline">
-            ข้อกำหนดและเงื่อนไขการให้บริการ
-          </Link>
-        </p>
-      </div>
-      <div className="flex items-center justify-center">
-        <button className="mx-auto w-[294px] bg-[var(--primary)] text-white font-bold py-2 mt-4  rounded-[var(--radius-lg)]">
-          สร้างบัญชีใหม่
-        </button>
-      </div>
+      <section className="mx-5">
+        <h2 className="mb-6">สามารถเลือกได้สูงสุด 5 ความสนใจ</h2>
+        <InterestsGrid interests={interests} />
+        <div className="flex items-center justify-center gap-2 mt-4">
+          <input type="checkbox" name="accept" />
+          <p>
+            <span className="opacity-70">ข้าพเจ้ายอมรับ</span>
+            &nbsp;
+            <Link href="#" className="hover:underline">
+              ข้อกำหนดและเงื่อนไขการให้บริการ
+            </Link>
+          </p>
+        </div>
+        <div className="flex items-center justify-center  mt-4">
+          <button className="w-full max-w-[294px] bg-[var(--primary)] text-white font-bold py-2  rounded-[var(--radius-lg)]">
+            สร้างบัญชีใหม่
+          </button>
+        </div>
+      </section>
     </main>
   );
 }
