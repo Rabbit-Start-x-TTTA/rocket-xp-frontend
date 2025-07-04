d2a6ca52815a4dcd0ce4095ad3550a5ce008ccfc
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-05 00:37:04 +0700

    feat: add term & condition checkbox and register button

--- src/app/onboarding/interests/page.tsx
+++ src/app/onboarding/interests/page.tsx
@@ -7,6 +7,15 @@ export default function InterestsPage() {
       <h1 className="text-xl font-bold bg-[#D9D9D9] text-center">ความสนใจ</h1>
       <h2 className="text-center">สามารถเลือกได้สูงสุด 5 ความสนใจ</h2>
       <InterestsGrid interests={interests} />
+      <div className="flex items-center justify-center gap-2 mt-4">
+        <input type="checkbox" name="accept" />
+        <p>
+          <span>ข้าพเจ้ายอมรับ</span>&nbsp;ข้อกำหนดและเงื่อนไขการให้บริการ
+        </p>
+      </div>
+      <button className="w-full bg-[#D9D9D9] text-black font-bold py-2 mt-4 rounded-[var(--radius-xl)]">
+        สร้างบัญชีใหม่
+      </button>
     </main>
   );
 }
