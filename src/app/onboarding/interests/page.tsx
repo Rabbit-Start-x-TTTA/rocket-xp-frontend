60112caa962863eb1c954f83e0ed394ad41aeb5c
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-05 00:01:39 +0700

    feat: add mockup data

--- src/app/onboarding/interests/page.tsx
+++ src/app/onboarding/interests/page.tsx
@@ -2,6 +2,7 @@ export default function InterestsPage() {
   return (
     <main className="flex min-h-screen">
       <h1>ความสนใจ</h1>
+      <h2>สามารถเลือกได้สูงสุด 5 ความสนใจ</h2>
     </main>
   );
 }
