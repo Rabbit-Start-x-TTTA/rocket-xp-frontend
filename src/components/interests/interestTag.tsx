ba3fed6057ec88bc8789db7ed473f167a7c6fa55
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-05 19:21:08 +0700

    feat: make selector can select 5 item

--- src/components/interests/interestTag.tsx
+++ src/components/interests/interestTag.tsx
@@ -1,20 +1,21 @@
-"use client";
-
 import { RiComputerFill } from "react-icons/ri";
-import { useState } from "react";
 
-export default function InterestTag({ interest }: { interest: string }) {
-  const [isSelected, setIsSelected] = useState(false);
-
+export default function InterestTag({
+  interest,
+  isSelected,
+  onClick,
+}: {
+  interest: string;
+  isSelected: boolean;
+  onClick: () => void;
+}) {
   return (
     <div
-      onClick={() => setIsSelected(!isSelected)}
+      onClick={onClick}
       className={`flex items-center justify-center gap-1.5 w-fit px-3.5 py-1 text-xs font-medium rounded-[var(--radius-xl)] border-2 cursor-pointer transition
-        ${isSelected ? "bg-black text-white " : null}`}
+        ${isSelected ? "bg-black text-white" : "bg-white text-black"}`}
     >
-      <div>
-        <RiComputerFill />
-      </div>
+      <RiComputerFill />
       {interest}
     </div>
   );
--- src/components/interests/interestsGrid.tsx
+++ src/components/interests/interestsGrid.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import InterestTag from "./interestTag";
 
 export default function InterestsGrid({
@@ -5,11 +8,28 @@ export default function InterestsGrid({
 }: {
   interests: { id: number; name: string }[];
 }) {
+  const [selected, setSelected] = useState<string[]>([]);
+  const maxSelected = 5;
+
+  function toggleInterest(interest: string) {
+    if (selected.includes(interest)) {
+      setSelected(selected.filter((item) => item !== interest));
+    } else if (selected.length < maxSelected) {
+      setSelected([...selected, interest]);
+    }
+  }
+
   return (
     <div className="flex flex-wrap gap-2 justify-center items-center mb-6">
       {interests.map((interest) => (
-        <InterestTag key={interest.id} interest={interest.name} />
+        <InterestTag
+          key={interest.id}
+          interest={interest.name}
+          isSelected={selected.includes(interest.name)}
+          onClick={() => toggleInterest(interest.name)}
+        />
       ))}
+      <p className="text-sm text-gray-600 w-full text-center"></p>
     </div>
   );
 }
