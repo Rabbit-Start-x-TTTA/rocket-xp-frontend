896c2923fa8881261d948585bd3c55f16a0d9fae
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-05 15:56:09 +0700

    style: remove border at main tag

--- src/app/onboarding/interests/page.tsx
+++ src/app/onboarding/interests/page.tsx
@@ -4,14 +4,14 @@ import Link from "next/link";
 
 export default function InterestsPage() {
   return (
-    <main className="min-h-screen max-w-[393px] mx-auto border-x">
+    <main className="min-h-screen max-w-[393px] mx-auto">
       <h1 className="text-xl font-bold bg-[#D9D9D9] text-center py-4 mb-2">
         ความสนใจ
       </h1>
       <section className="mx-5">
         <h2 className="mb-6">สามารถเลือกได้สูงสุด 5 ความสนใจ</h2>
         <InterestsGrid interests={interests} />
-        <div className="flex items-center justify-center gap-2 mt-4">
+        <div className="flex items-center justify-center gap-2 mt-4 text-sm">
           <input type="checkbox" name="accept" />
           <p>
             <span className="opacity-70">ข้าพเจ้ายอมรับ</span>
@@ -22,7 +22,7 @@ export default function InterestsPage() {
           </p>
         </div>
         <div className="flex items-center justify-center  mt-4">
-          <button className="w-full max-w-[294px] bg-[var(--primary)] text-white font-bold py-2  rounded-[var(--radius-lg)]">
+          <button className="w-full max-w-[294px] bg-[var(--primary)] text-white py-2  rounded-[var(--radius-lg)]">
             สร้างบัญชีใหม่
           </button>
         </div>
