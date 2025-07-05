4ec89104e07362101a7945c3e8ff267d37cf59c7
Bazkiller <ohm.ratchaphon@gmail.com>
2025-07-05 16:16:19 +0700

    style: change style when tag was clicked

--- src/components/interests/interestTag.tsx
+++ src/components/interests/interestTag.tsx
@@ -1,8 +1,17 @@
+"use client";
+
 import { RiComputerFill } from "react-icons/ri";
+import { useState } from "react";
 
 export default function InterestTag({ interest }: { interest: string }) {
+  const [isSelected, setIsSelected] = useState(false);
+
   return (
-    <div className="flex items-center justify-center gap-1.5 w-fit px-3.5 py-1 text-xs font-medium  border-2 border-[var(--border)] rounded-[var(--radius-xl)]">
+    <div
+      onClick={() => setIsSelected(!isSelected)}
+      className={`flex items-center justify-center gap-1.5 w-fit px-3.5 py-1 text-xs font-medium rounded-[var(--radius-xl)] border-2 cursor-pointer transition
+        ${isSelected ? "bg-black text-white " : null}`}
+    >
       <div>
         <RiComputerFill />
       </div>
